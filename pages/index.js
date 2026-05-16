f65dec13b066789a111e4de4f9dbba6d4190d77a
phanindraeswar-svg <phanindra.eswar@gmail.com>
2026-05-16 21:29:59 +0530

    Add Home component with welcome message

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ fontFamily: 'sans-serif', padding: '40px', textAlign: 'center' }}>
+      <h1>Welcome to BharathQR</h1>
+      <p>Your automated SEO directory is active.</p>
+    </div>
+  );
+}
